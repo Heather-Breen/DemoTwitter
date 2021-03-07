64ed647715a3d2a32795268682a9fdd473b7900f
HeatherBreen <heatherhcx@gmail.com>
2021-03-07 11:38:14 -0600

    End of Class MAR 4

--- src/main/java/com/example/demoTwitter/model/User.java
+++ src/main/java/com/example/demoTwitter/model/User.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-//import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
@@ -15,38 +14,41 @@ import javax.validation.constraints.Pattern;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
-
+// left off slide 41
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+// This is the user class that allows the user to be created.
 public class User {
-    //database
+    // this here allows the Id to be auto generated
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private Long id;
+// setting up the variables that we will need
 
     @Email(message = "Please provide a valid email")
     @NotEmpty(message = "Please provide an email")
-
-    @Length(min = 3, message="your username must have more than 3 letters")
-    @Length(max = 15, message="your username cannot have more than 15 letters")
-    @Pattern(regexp="[^\\s]+", message="Your username cannot contain spaces")
-    @Length(min = 5, message="Your password must be more than 5 letters")
-    @Length(message="Can't leave empty")
-
     private String email;
+
+    @Length(min = 3, message="your username must have more then 3 letters")
+    @Length(max = 15, message = "Your username cannot have more than 15 characters")
+    @Pattern(regexp="[^\\s]+", message="Your username cannot contain spaces")
     private String username;
+    @Length(min = 5, message="Your password needs to be more than 5 characters")
     private String password;
+    @Length(message="Cant leave empty")
     private String firstName;
+    @Length(message="Cant leave empty")
     private String lastName;
     private int active;
 
     @CreationTimestamp
     private Date createdAt;
-
+    // that start of the sql querys to line the user_id to the roles and the following.
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
